749a2491a46fcd08d6d6460de3a28c7cf6418b19
arunkraveendran <36581153+arunkraveendran@users.noreply.github.com>
2018-02-20 22:25:38 +0530

    Initial version

---
+++ uno_based/plantcare_uno/plantcare_uno.ino
@@ -0,0 +1,23 @@
+int ledPinOut = 13;
+int ledPinIn = 14;
+
+void setup()
+{
+  pinMode(ledPinOut, OUTPUT);
+  pinMode(ledPinIn, INPUT);
+} 
+
+
+void loop()
+{
+  // if Moisture is less than threshold
+  if(ledPinIn == 1)
+  {
+    digitalWrite(ledPinOut, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPinOut, LOW);
+  }
+
+} 
