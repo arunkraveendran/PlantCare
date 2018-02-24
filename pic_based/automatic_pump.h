6e4d853ad8b19c7a4c9700e4f2a6beff33806dcc
arunkraveendran <36581153+arunkraveendran@users.noreply.github.com>
2018-02-24 12:40:20 +0530

    Inital Version

--- pic_based/automatic_pump.h
+++ pic_based/automatic_pump.h
@@ -1,3 +1,34 @@
 #ifdef MODULE_HEADER
-
+//---------------------------------------------------------------------------//
+//                    Copy right @ Arun 2018                                 //
+//---------------------------------------------------------------------------//
+//                                                                           //
+// Date         Name                 Description                             //
+//---------------------------------------------------------------------------//
+// 24Feb18      Arun                 Initial Version                         //
+//---------------------------------------------------------------------------//
 #endif
+
+
+#include<pic16f877a.h>
+#define _XTAL_FREQ 8000000
+
+void ADC_Init()
+{
+   ADCON0 = 0x41;
+   ADCON1 = 0xC0;
+}
+
+unsigned int ADC_Read(unsigned char channel)
+{
+   if(channel > 7)
+   {
+       return 0; 
+   }
+   ADCON0 &= 0xC5;
+   ADCON0 |= channel<<3;
+   GO_nDONE = 1;
+   while(GO_nDONE);
+   return ((ADRESH<<8)+ADRESL);
+}
+
